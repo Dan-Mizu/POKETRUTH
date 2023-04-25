8b79f9e5a866ff14adc3376755d86c8b9e73e356
Dan <danmizu@nostalgi.cc>
2023-04-25 00:21:59 -0700

    feat: vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
@@ -7,7 +8,12 @@ import '@/styles/globals.css';
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
