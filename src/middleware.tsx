671b9da56b5b60bfae0aba63c7bb1e9796b4a4c2
Dan <danmizu@nostalgi.cc>
2023-08-18 01:21:01 -0700

    ignore public folder while routing

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -10,3 +10,18 @@ export function middleware(request: NextRequest) {
     }`
   );
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - .*\\..* (url.extension files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - robots.txt (robots file)
+     */
+    '/((?!api|_next/static|.*\\..*|_next/image|favicon.ico|robots.txt).*)',
+  ],
+};
