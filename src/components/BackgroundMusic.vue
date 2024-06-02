67dde86e6ba79e0ead53c2620a484f3098f7c066
Dan <business@danmizu.dev>
2024-06-02 16:10:24 -0700

    feat: new music for take over

---
+++ src/components/BackgroundMusic.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// properties
+const props = defineProps<{
+	music: string;
+}>();
+
+// play music
+if (process.client) {
+	(document.getElementById("backgroundMusic") as HTMLAudioElement)
+		.play()
+		.catch((_error) => {
+			document.addEventListener(
+				"click",
+				() => {
+					(
+						document.getElementById(
+							"backgroundMusic"
+						) as HTMLAudioElement
+					).play();
+				},
+				{ once: true }
+			);
+		});
+}
+</script>
+
+<template>
+	<!-- Music -->
+	<audio id="backgroundMusic" loop autoPlay defaultValue="{0.25}">
+		<source :src="props.music" type="audio/ogg" />
+	</audio>
+</template>
