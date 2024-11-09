0ccacae6682741ddd2085e11db33afedca5132bf
Dan <business@danmizu.dev>
2024-11-08 22:05:26 -0800

    update card demo

--- server/routes/cards.ts
+++ server/routes/cards.ts
@@ -1,5 +1,5 @@
 import fs from "node:fs";
 
 export default defineEventHandler(() => {
-	return fs.readFileSync("./src/public/CardDemo.html");
+	return fs.readFileSync("./src/public/cards/index.html");
 });
